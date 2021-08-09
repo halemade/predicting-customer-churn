cf5e1bc50c1bdafa8fffa4eb171c16ee469427d2
Taylor Hale Robert <taylorhale11@gmail.com>
2021-08-09 19:04:26 -0400

    more work on final model

--- functions.py
+++ functions.py
@@ -183,3 +183,83 @@ def plot_importances(model_dict,X):
     fig = px.bar(sort, x="features", y="importances", barmode="group")
     fig.update_layout(title = 'XGBoost Feature Importances')    
     return fig.show()
+
+def plot_roc(results):
+    #plot ROC curve
+    kind = 'val'
+    c_fill      = 'rgba(52, 152, 219, 0.2)'
+    c_line      = 'rgba(52, 152, 219, 0.5)'
+    c_line_main = 'rgba(41, 128, 185, 1.0)'
+    c_grid      = 'rgba(189, 195, 199, 0.5)'
+    c_annot     = 'rgba(149, 165, 166, 0.5)'
+    c_highlight = 'rgba(192, 57, 43, 1.0)'
+    fpr_mean    = np.linspace(0, 1, 100)
+    interp_tprs = []
+    for i in range(100):
+        fpr           = results[kind]['fpr'][i]
+        tpr           = results[kind]['tpr'][i]
+        interp_tpr    = np.interp(fpr_mean, fpr, tpr)
+        interp_tpr[0] = 0.0
+        interp_tprs.append(interp_tpr)
+    tpr_mean     = np.mean(interp_tprs, axis=0)
+    tpr_mean[-1] = 1.0
+    tpr_std      = 2*np.std(interp_tprs, axis=0)
+    tpr_upper    = np.clip(tpr_mean+tpr_std, 0, 1)
+    tpr_lower    = tpr_mean-tpr_std
+    auc          = np.mean(results[kind]['auc'])
+    fig = go.Figure([
+        go.Scatter(
+            x          = fpr_mean,
+            y          = tpr_upper,
+            line       = dict(color=c_line, width=1),
+            hoverinfo  = "skip",
+            showlegend = False,
+            name       = 'upper'),
+        go.Scatter(
+            x          = fpr_mean,
+            y          = tpr_lower,
+            fill       = 'tonexty',
+            fillcolor  = c_fill,
+            line       = dict(color=c_line, width=1),
+            hoverinfo  = "skip",
+            showlegend = False,
+            name       = 'lower'),
+        go.Scatter(
+            x          = fpr_mean,
+            y          = tpr_mean,
+            line       = dict(color=c_line_main, width=2),
+            hoverinfo  = "skip",
+            showlegend = True,
+            name       = f'AUC: {auc:.3f}')
+    ])
+    fig.add_shape(
+        type ='line', 
+        line =dict(dash='dash'),
+        x0=0, x1=1, y0=0, y1=1
+    )
+    fig.update_layout(
+        template    = 'plotly_white', 
+        title_x     = 0.5,
+        xaxis_title = "1 - Specificity",
+        yaxis_title = "Sensitivity",
+        width       = 800,
+        height      = 800,
+        legend      = dict(
+            yanchor="bottom", 
+            xanchor="right", 
+            x=0.95,
+            y=0.01,
+        )
+    )
+    fig.update_yaxes(
+        range       = [0, 1],
+        gridcolor   = c_grid,
+        scaleanchor = "x", 
+        scaleratio  = 1,
+        linecolor   = 'black')
+    fig.update_xaxes(
+        range       = [0, 1],
+        gridcolor   = c_grid,
+        constrain   = 'domain',
+        linecolor   = 'black')
+    return fig.show()
